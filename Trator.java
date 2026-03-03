9c1240c21716cb9d112dd58ac3bcb7c6d3dcef5f
Pedro Paulo Mardegam <pedro.paulo.mardegam@gmail.com>
2026-03-03 20:35:20 -0300

    Update Trator.java
    fix

--- Trator.java
+++ Trator.java
@@ -1,14 +1,31 @@
-package main;
+package main.java;
 
 public class Trator extends Carro {
+	
+	private String motor;
+	private String modelo;
 
-	public Trator(String marca, String cor, int ano) {
+	public Trator(String marca, String cor, int ano, String motor, String modelo) {
 		super(marca, cor, ano);
 		// TODO Auto-generated constructor stub
 		
+		this.motor=motor;
+		this.modelo=modelo;
 	}
 	
+	public String getMotor() {
+		return	motor;
+	}
 	
+	public String getModelo() {
+		return modelo;
+	}
+	
+	@Override
+	public String buzina() {
+		
+		return ("FOOOOONFOOOON");
+	}
 	
 
 }
