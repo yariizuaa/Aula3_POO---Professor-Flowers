c2b0166bb3e7c2a78ebb9003b18427c648fde7ef
Pedro Paulo Mardegam <pedro.paulo.mardegam@gmail.com>
2026-03-03 20:34:50 -0300

    Update Main.java
    Fix

--- Main.java
+++ Main.java
@@ -1,28 +1,31 @@
-package main;
+package main.java;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
 		
 		System.out.println("Hello World");
 		
 		
 		Carro carro1 = new Carro("Honda Civic","Preto", 2020);
-		System.out.println("A marca do carro é" + carro1.getMarca());
+		System.out.println("A marca do carro é " + carro1.getMarca());
 		
-		Trator trator1 = new Trator("Volvo", "Preto", 1988);
-		System.out.println("A marca do Trator é" + trator1.getMarca());
-
-		System.out.println (carro1.buzina());
-		System.out.println (carro1.buzina());
-		System.out.println (carro1.buzina());
-		System.out.println (carro1.buzina());
-		System.out.println (carro1.buzina());
-		System.out.println (carro1.buzina());
-		System.out.println (carro1.buzina());
-		System.out.println (carro1.buzina());
+		Trator trator1 = new Trator("Volvo", "Preto", 1988, "Super Green", "TR450");
+		System.out.println("A marca do Trator é " + trator1.getMarca());
+		System.out.println("O motor do Trator é " + trator1.getMotor());
+		System.out.println("O modelo do Trator é " + trator1.getModelo());
+		System.out.println("A cor do Trator é " + trator1.getCor());
+		System.out.println("O ano do Trator é " + trator1.getAno());
 		
+		
+		System.out.println (carro1.buzina());
+		System.out.println (carro1.buzina());
+		System.out.println (carro1.buzina());
+		System.out.println (carro1.buzina());
+		System.out.println (carro1.buzina());
+		System.out.println (carro1.buzina());
+		System.out.println (carro1.buzina());
+		System.out.println (carro1.buzina());
 	}
-
 }
